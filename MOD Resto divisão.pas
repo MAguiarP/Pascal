de8342a0cd0ea9afb1ea340c5c545f40a0e3bea4
MAguiarP <matheusdeaguiar@live.com>
2022-09-26 14:46:43 -0300

    Create MOD Resto divisão.pas

---
+++ MOD Resto divisão.pas
@@ -0,0 +1,12 @@
+Program Expressoes;
+
+Var N1,N2, Conta: integer; 
+
+Begin
+  writeln('Digite N1');
+  Readln(N1);                                                            
+  writeln('Digite N2');
+  Readln(N2);
+  Conta :=N1 mod N2;
+  writeln('A conta é: ' , Conta);
+End.      
