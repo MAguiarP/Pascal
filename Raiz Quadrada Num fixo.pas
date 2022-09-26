151a4f82e908c88ae4f28873feff60f49d43604b
MAguiarP <matheusdeaguiar@live.com>
2022-09-26 15:42:48 -0300

    Create Raiz Quadrada Num fixo.pas

---
+++ Raiz Quadrada Num fixo.pas
@@ -0,0 +1,12 @@
+Program raiz_quadrada;
+
+	var Resultado1,Resultado2: Real;
+	
+Begin
+ Resultado1:=Sqrt(9);
+ Resultado2:=Sqrt(81);
+ Writeln('A Raiz quadrada de 9 é: ' , Resultado1 :2:2);
+ Writeln('');
+ Writeln('A Raiz quadrada de 81 é: ' , Resultado2 :2:2);
+ Readln; 
+End.
