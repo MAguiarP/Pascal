cf4e3813884c7c98b65992408048494abb9ae20f
MAguiarP <matheusdeaguiar@live.com>
2022-09-26 15:19:30 -0300

    Create Soma 4 Num.pas

---
+++ Soma 4 Num.pas
@@ -0,0 +1,16 @@
+Program Expressoes;
+
+Var N1,N2,N3,N4, Conta: integer; 
+
+Begin
+  writeln('Digite N1');
+  Readln(N1);                                                            
+  writeln('Digite N2');
+  Readln(N2);
+  writeln('Digite N3');
+  Readln(N3);                                                            
+  writeln('Digite N4');
+  Readln(N4);
+  Conta :=N1 + N2 + N3 + N4;
+  writeln('A conta é: ' , Conta);
+End.      
