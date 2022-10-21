8515e0f98d1d2f8e75da490729d52e034ed4e025
Matheus de Aguiar <102604133+MAguiarP@users.noreply.github.com>
2022-10-21 14:33:54 -0300

    001

---
+++ Pojeto01.pas
@@ -0,0 +1,14 @@
+Program projeto1 ;
+var Nome,Idade:string;
+
+Begin
+	Writeln('Digite seu Nome');
+	Readln(Nome);
+	Writeln('Digite sua Idade');
+	Readln(Idade);
+	Writeln('Meu Nome é: ', Nome);
+	Writeln('Tenho ', Idade, ' Anos e esse é meu primeiro projeto em Pascal');
+	readln;
+	
+	 
+End.
