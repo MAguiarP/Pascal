b5f23af4cb702b465b8fa494ab0f57b447a1f97e
Matheus de Aguiar <102604133+MAguiarP@users.noreply.github.com>
2022-10-21 15:02:37 -0300

    ex

---
+++ Peso.pas
@@ -0,0 +1,12 @@
+Program Peso ;
+	var Peso1,Peso2,Soma:Real;
+	
+Begin
+  writeln('Digite o Peso da Primeira Pessoa');
+  readln(Peso1);
+  writeln('DIgite o Peso da Segunda Pessoa');
+  readln(Peso2);
+  Soma:=Peso1 + Peso2;
+  writeln('A soma dos pesos da duas pessoas é ', Soma :2:2);
+  readln;
+End.
---
+++ Tabuada.pas
@@ -0,0 +1,17 @@
+Program Tabuada ;
+var Numero:Real;
+Begin
+  
+  Numero:=10;
+  writeln('1 x ', Numero :2:2, '=', numero*1 :2:2);
+  writeln('1 x ', Numero :2:2, '=', numero*2 :2:2);
+  writeln('1 x ', Numero :2:2, '=', numero*3 :2:2);
+  writeln('1 x ', Numero :2:2, '=', numero*4 :2:2);
+  writeln('1 x ', Numero :2:2, '=', numero*5 :2:2);
+  writeln('1 x ', Numero :2:2, '=', numero*6 :2:2);
+  writeln('1 x ', Numero :2:2, '=', numero*7 :2:2);
+  writeln('1 x ', Numero :2:2, '=', numero*8 :2:2);
+  writeln('1 x ', Numero :2:2, '=', numero*9 :2:2);
+  writeln('1 x ', Numero :2:2, '=', numero*10 :2:2);
+  
+End.            	
