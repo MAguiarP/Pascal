5cadc01feb98fe82485f3630e9dc09301cc7917b
Matheus de Aguiar <102604133+MAguiarP@users.noreply.github.com>
2022-11-10 10:24:34 -0300

    Create arrayidade.pas

---
+++ arrayidade.pas
@@ -0,0 +1,12 @@
+program idades;
+ var
+ 	idade:array[1..5] of integer;
+ 	i:integer;
+	
+	begin
+		for i:= 1 to 5 do
+			begin
+				writeln('informe Idade',24i);
+				readln(idade[i]);
+			end;
+		end.
